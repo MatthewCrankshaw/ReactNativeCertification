bc85956d7d6a2a26451a1e7c91e62c66ef06d370
Matthew Crankshaw <matthew.cranskhaw@ezyvet.com>
2021-04-06 13:36:44 +1200

    initial commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+ import React from 'react';
+ import {
+   SafeAreaView,
+   StatusBar,
+   Text,
+   View,
+ } from 'react-native';
+
+ const App = () => {
+
+   return (
+     <SafeAreaView>
+       <StatusBar />
+         <View>
+           <Text>
+             Hello world
+           </Text>
+         </View>
+     </SafeAreaView>
+   );
+ };
+
+ export default App;
