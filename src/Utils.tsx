f1bb6cec3178c221e1341cd7700eca1d72a3324b
Matthew Crankshaw <matthew.cranskhaw@ezyvet.com>
2021-04-20 09:31:04 +1200

    docs: added documentation to utils

--- src/Utils.tsx
+++ src/Utils.tsx
@@ -1,3 +1,10 @@
+/**
+ * Convert a numeric value into string money format
+ *
+ * @param {number} value The value we would like in money format
+ *
+ * @return {string}
+ */
 export const moneyFormat = (value: number) => {
   return '$' + value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 };
